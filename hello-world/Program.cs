80679c0f03b3ae211beffc23061021e772285a14
JhersonRojas <jhersonrojas.dev@gmail.com>
2023-12-13 08:04:27 -0500

    primer programa con csharp

---
+++ hello-world/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+// Nombre del area del proyecto
+namespace HelloWorld
+{
+  class MainClass
+  {
+    static void Main(string[] args)
+    {
+      // Display the number of command line arguments.
+      Console.WriteLine("Hello World desde Csharp y .NET 8");
+    }
+  }
+}
